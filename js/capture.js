d0016074ac4174ad6c4c9889efbd0e841c42926f
TevinStuurland <tevin94@hotmail.com>
2017-10-11 16:45:20 +0200

    fixing the application

--- js/capture.js
+++ js/capture.js
@@ -22,7 +22,7 @@
   function startup() {
     video = document.querySelector('video');
     canvas = document.querySelector('canvas');
-    photo = document.getElementById('photo');
+    photo = document.querySelector('photo');
     startbutton = document.querySelector('startbutton');
 
     navigator.getMedia = ( navigator.getUserMedia ||
